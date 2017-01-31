8e5b3c1675b6fe2d503b3a2572f387b1221242b9
J2A2B <ja.andreani@gmail.com>
2017-01-31 19:47:05 +0100

    commit to pull

--- src/app/js/controllers/profilUserEntreprise.js
+++ src/app/js/controllers/profilUserEntreprise.js
@@ -2,5 +2,38 @@ app.controller('profilUserEntreprise',['$http', '$scope', '$rootScope', 'AuthSer
 
 	$scope.member = AuthService.user();
 	console.log($scope.member)
+
+	$scope.updateEntreprise = (id) => {
+          const response = confirm("Voulez vous vraiment modifier vos informations ?");
+          if (response === true) {
+              const newInfos = {
+                  company: $scope.member.company,
+                  firstName: $scope.member.firstName,
+                  lastName: $scope.member.lastName,
+                  ville: $scope.student.ville,
+                  photo: $scope.student.photo,
+                  tags: $scope.student.tags,
+                  description: $scope.student.description,
+                  Sexe: $scope.student.Sexe,
+                  SpecialiteUn: $scope.student.SpecialiteUn,
+                  SpecialiteDeux: $scope.student.SpecialiteDeux,
+                  SpecialiteTrois: $scope.student.SpecialiteTrois,
+                  Github: $scope.student.Github,
+                  Linkedin: $scope.student.Linkedin,
+                  Portfolio: $scope.student.Portfolio,
+                  CV: $scope.student.CV,
+                  Twitter: $scope.student.Twitter,
+                  StackOverFlow: $scope.student.StackOverFlow,
+                  Mail: $scope.student.Mail,
+                  Contrat: $scope.student.Contrat,
+                  DatePromo: $scope.student.DatePromo,
+                  Domaine: $scope.student.Domaine
+
+              };
+
+              serviceEntreprise.updateEntreprise(id, newInfos).then(alert("Vos informations on bien été mises à jour !"))
+          };
+          $state.reload();
+      };
     }    
 ]);
