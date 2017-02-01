f9f7b72ed7dcc04b77241ab179bbc5a4cedca281
J2A2B <ja.andreani@gmail.com>
2017-02-01 17:14:10 +0100

    profi recruiter

--- src/app/js/controllers/profilUserEntreprise.js
+++ src/app/js/controllers/profilUserEntreprise.js
@@ -1,24 +1,51 @@
 app.controller('profilUserEntreprise',['$http', '$scope', '$rootScope', 'AuthService', '$state', '$window', 'serviceStudent', 'AuthService', function($http, $scope, $rootScope, AuthService, $state, $window, serviceStudent, AuthService) {
-
+      $scope.anime = true;
+      $scope.validate = true;
 	$scope.member = AuthService.user();
-	console.log(AuthService.user());
-
+      
 	$scope.updateUser = (id) => {
+          // console.log($scope.anime)
           const response = confirm("Voulez vous vraiment modifier vos informations ?");
           if (response === true) {
+            $scope.validate = false;
               const newInfos = {
                   company: $scope.member.company,
                   firstName: $scope.member.firstName,
                   lastName: $scope.member.lastName,
-                  email: $scope.member.email,
-                  password: $scope.member.password
+                  email: $scope.member.email
 
               };
 
               AuthService.updateUser(id, newInfos).then((res) => {
-								alert("Vos informations on bien été mises à jour !");
+			alert("Vos informations on bien été mises à jour !");
 		})
           };
       };
+      $scope.updateMdp = (id) => {
+          //const response = confirm("Voulez vous vraiment modifier vos informations ?");
+          //if (response === true) {
+              const newInfos = {
+                  company: $scope.member.company,
+                  firstName: $scope.member.firstName,
+                  lastName: $scope.member.lastName,
+                  email: $scope.member.email
+
+              };
+
+              AuthService.updateUser(id, newInfos).then((res) => {
+                //alert("Vos informations on bien été mises à jour !");
+    })
+          //};
+      };
+
+      $scope.changeView = () =>{
+        $scope.validate = true;
+        $scope.anime = !$scope.anime;
+        if ($scope.anime === false ) {
+          $('.button-change-view').val("Gérer mes informations personnelles").delay(80000);
+        }else if($scope.anime === true){
+        $('.button-change-view').val("Gérer mon mot de passe");
+      }
+      }
     }
 ]);
