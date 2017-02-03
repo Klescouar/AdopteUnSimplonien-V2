02891323a9d46904f51889dda4965eaf4b6c8789
Poncho <lescouarneckevin@yahoo.fr>
2017-02-03 11:17:44 +0100

    pull to push

--- src/app/js/controllers/profils-userCtrl.js
+++ src/app/js/controllers/profils-userCtrl.js
@@ -1,4 +1,4 @@
-app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService', '$state', '$window', 'serviceStudent', 'serviceFilter', function($http, $scope, $rootScope, AuthService, $state, $window, serviceStudent, serviceFilter) {
+app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService', '$state', '$window', 'serviceStudent', 'serviceFilter', 'AuthService', function($http, $scope, $rootScope, AuthService, $state, $window, serviceStudent, serviceFilter, AuthService) {
         $scope.member = AuthService.user();
         $scope.showEditProfilUser = false;
         $scope.photo = '';
@@ -9,6 +9,54 @@ app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService'
         if (!$scope.student.tags) {
           $scope.student.tags = [];
         }
+
+        $scope.newPassword = {
+            newpass: '',
+            oldpass: ''
+        };
+
+        $scope.member = AuthService.user();
+
+
+        $scope.updateUser = (id) => {
+            const response = confirm("Voulez vous vraiment modifier vos informations ?");
+            if (response === true) {
+                $scope.validate = false;
+                const newInfos = {
+                    company: $scope.member.company,
+                    firstName: $scope.member.firstName,
+                    lastName: $scope.member.lastName,
+                    email: $scope.member.email
+                }
+                AuthService.updateUser(id, newInfos).then((res) => {
+
+                    alert("Vos informations on bien été mises à jour !");
+
+                })
+            }
+        }
+
+
+        $scope.updateUserPass = (id, newPassword) => {
+                 if (newPassword.newpass.trim().length >= 8) {
+                    if (newPassword.newpass === $scope.passwordChecked) {
+          AuthService.updateUserPassFromProfil(id, newPassword).then((res) => {
+                        if (res.data.msg === 'Wrong password') {
+            $scope.oldPassVerif = false;
+                        }else{
+                          $scope.oldPassVerif = true;
+                          $scope.validate = false;
+                          $scope.newPassword = {
+                            newpass: '',
+                            oldpass: ''
+                          };
+                          $scope.passwordChecked = '';
+                        }
+          });
+                  }
+              }
+          }
+
 
 
 
