2908578731b1fb97072905eba1bcaa778eaa69bc
Poncho <lescouarneckevin@yahoo.fr>
2017-02-03 12:19:07 +0100

    Add fixed position to profil background

--- src/app/js/controllers/profils-userCtrl.js
+++ src/app/js/controllers/profils-userCtrl.js
@@ -4,7 +4,7 @@ app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService'
         $scope.photo = '';
         $scope.turnOff = false;
         $scope.student = {};
-        $scope.tab = 'infos';
+        $scope.tab = 'fiche';
 
         if (!$scope.student.tags) {
           $scope.student.tags = [];
