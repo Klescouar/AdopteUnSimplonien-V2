6a23fbf35b7a2b05721e404ab1a85137b18d0b0b
J2A2B <ja.andreani@gmail.com>
2017-02-05 12:31:46 +0100

    bug signup signin

--- src/app/js/controllers/profilUserEntreprise.js
+++ src/app/js/controllers/profilUserEntreprise.js
@@ -18,50 +18,61 @@ app.controller('profilUserEntreprise', [
             		$scope.oldPassVerif = true;
 		$scope.member = AuthService.user();
 
-		$scope.updateUser = (id) => {
-		$scope.validate = true;
-		    const response = confirm("Voulez vous vraiment modifier vos informations ?");
-		    if (response === true) {
-		        $scope.validate = false;
-		        const newInfos = {
-		            company: $scope.member.company,
-		            firstName: $scope.member.firstName,
-		            lastName: $scope.member.lastName,
-		            email: $scope.member.email
-		        }
-		        AuthService.updateUser(id, newInfos).then((res) => {
+	// ANIMATION CHANGING VIEW
 
-		            alert("Vos informations on bien été mises à jour !");
+	$scope.changeView = () => {
+                $scope.oldPassVerif = true;
+               	$scope.validate = true;
+		   $scope.anime = !$scope.anime;
+		   if ($scope.anime === false) {
+		       $('.button-change-view').val("Gérer mes informations personnelles");
+		   } else if ($scope.anime === true) {
+		       $('.button-change-view').val("Gérer mon mot de passe");
+		   }
+	}
 
-		        })
-		    }
-		}
-		$scope.changeView = () => {
-                	$scope.oldPassVerif = true;
-               	 $scope.validate = true;
-		    $scope.anime = !$scope.anime;
-		    if ($scope.anime === false) {
-		        $('.button-change-view').val("Gérer mes informations personnelles");
-		    } else if ($scope.anime === true) {
-		        $('.button-change-view').val("Gérer mon mot de passe");
-		    }
-		}
+	// UPDATE INFORMATIONS USER
+
+	$scope.updateUser = (id) => {
+	$scope.validate = true;
+		 const response = confirm("Voulez vous vraiment modifier vos informations ?");
+		 if (response === true) {
+		     $scope.validate = false;
+		     const newInfos = {
+		         company: $scope.member.company,
+		         firstName: $scope.member.firstName,
+		         lastName: $scope.member.lastName,
+		         email: $scope.member.email
+		     }
+		     AuthService.updateUser(id, newInfos).then((res) => {
+
+		         alert("Vos informations on bien été mises à jour !");
+
+		      })
+		 }
+	}
+
+	// UPDATE PASSWORD USER
+
 	$scope.updateUserPass = (id, newPassword) => {
 	$scope.validate = true;
              if (newPassword.newpass.trim().length >= 8) {
                 if (newPassword.newpass === $scope.passwordChecked) {
 		AuthService.updateUserPassFromProfil(id, newPassword).then((res) => {
+		console.log("validé1")
                     if (res.data.msg === 'Wrong password') {
+                    	console.log("NONvalidé1")
 		$scope.oldPassVerif = false;
                     }else{
+                    	console.log("validé2")
                     	$scope.oldPassVerif = true;
-                      $scope.validate = false;
-                      $scope.newPassword = {
-                        newpass: '',
-                        oldpass: ''
-                      };
-                      $scope.passwordChecked = '';
-                    }
+                      	$scope.validate = false;
+                      	$scope.newPassword = {
+                        		newpass: '',
+                        		oldpass: ''
+                      	};
+                      	$scope.passwordChecked = '';
+                    	}
 		});
               }
           }
--- src/app/js/controllers/projectCtrl.js
+++ src/app/js/controllers/projectCtrl.js
@@ -10,15 +10,15 @@ app.controller('projectCtrl', ['$scope', '$http', 'serviceFilter', '$timeout', '
 
 
      angular.element($window).bind("scroll", (e) => {
-        if(window.pageYOffset > 40){
+        if(window.pageYOffset > 350){
             $scope.showTitle = false; 
-        }else if(window.pageYOffset < 40){
+        }else if(window.pageYOffset < 350){
             $scope.showTitle = true;  
         }
-        if (window.pageYOffset > 600) {
+        if (window.pageYOffset > 900) {
              $scope.showSimplon = false;
          }
-         else if (window.pageYOffset < 500) {
+         else if (window.pageYOffset < 900) {
            $scope.showSimplon = true;
          }
          $scope.$apply(() => {});
