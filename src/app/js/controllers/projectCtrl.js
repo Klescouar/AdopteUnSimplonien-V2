7a9acea5bf401627cd9d45c2993e2763191fb83f
J2A2B <ja.andreani@gmail.com>
2017-02-05 15:55:23 +0100

    opacity project+ clean files

--- src/app/js/controllers/projectCtrl.js
+++ src/app/js/controllers/projectCtrl.js
@@ -1,13 +1,6 @@
 app.controller('projectCtrl', ['$scope', '$http', 'serviceFilter', '$timeout', '$window', function($scope, $http, serviceFilter, $timeout, $window){
     $scope.showTitle = true;
     $scope.showSimplon = true;
-    // var positionSimplonien = $("#simplonien").offset().top;
-    // var positionSimplon = $("#simplon").offset().top;
-    // console.log(positionSimplonien)
-    // console.log(positionSimplon)
-    //var screen = window.pageYOffset;
-    //console.log(screen)
-
 
      angular.element($window).bind("scroll", (e) => {
         if(window.pageYOffset > 350){
@@ -23,32 +16,4 @@ app.controller('projectCtrl', ['$scope', '$http', 'serviceFilter', '$timeout', '
          }
          $scope.$apply(() => {});
      });
-
-
-
-    // $scope.showDescription = false;
-    // $scope.showDescription2 = false;
-
-    // $timeout(() => {
-    //     if (window.innerWidth < 640) {
-    //         $scope.showDescription = true;
-    //         $scope.showDescription2 = true;
-    //     }
-    // }, 70);
-
-    // angular.element($window).bind("scroll", (e) => {
-    //     if (window.pageYOffset > 40) {
-    //         $scope.showDescription = true;
-    //     }
-    //     else if (window.pageYOffset < 40) {
-    //       $scope.showDescription = false ;
-    //     }
-    //     if (window.pageYOffset > 600) {
-    //         $scope.showDescription2 = true;
-    //     }
-    //     else if (window.pageYOffset < 500) {
-    //       $scope.showDescription2 = false;
-    //     }
-    //     $scope.$apply(() => {});
-    // });
 }]);
