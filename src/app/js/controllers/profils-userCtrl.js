14d25ab7723ae6fbd3bc8a99414e265f95b2926f
Yacine CDJ <y.chibane@me.com>
2017-02-12 12:16:06 +0100

    validation info + password OK, fiche 80%

--- src/app/js/controllers/profils-userCtrl.js
+++ src/app/js/controllers/profils-userCtrl.js
@@ -39,7 +39,6 @@ app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService'
       AuthService.updateUserPassFromProfil(id, $scope.newPassword).then((res) => {
         if (res.data.msg === 'Wrong password') {
           alert('Mauvais mot de passe');
-          $scope.passwordCheck = false;
         } else {
           alert('Mot de passe modifié!')
         }
@@ -118,32 +117,32 @@ app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService'
 
   $scope.createSimplonien = () => {
     const dataStudent = {
-      memberId: $scope.member._id,
-      verified: false,
-      nom: $scope.student.nom,
-      prenom: $scope.student.prenom,
-      age: $scope.student.age,
-      ville: $scope.student.ville,
-      photo: $scope.photo,
-      tags: $scope.student.tags,
-      description: $scope.student.description,
-      Sexe: $scope.student.Sexe,
-      SpecialiteUn: $scope.student.SpecialiteUn,
-      SpecialiteDeux: $scope.student.SpecialiteDeux,
-      SpecialiteTrois: $scope.student.SpecialiteTrois,
-      Github: $scope.student.Github,
-      Linkedin: $scope.student.Linkedin,
-      Portfolio: $scope.student.Portfolio,
-      CV: $scope.student.CV,
-      Twitter: $scope.student.Twitter,
-      StackOverFlow: $scope.student.StackOverFlow,
-      Mail: $scope.student.Mail,
-      Contrat: $scope.student.Contrat,
-      DatePromo: $scope.student.DatePromo,
-      Domaine: $scope.student.Domaine,
-      ProjetUn: $scope.student.ProjetUn,
-      ProjetDeux: $scope.student.ProjetDeux,
-      ProjetTrois: $scope.student.ProjetTrois
+    memberId: $scope.member._id,
+    verified: false,
+    nom: $scope.member.lastName,
+    prenom: $scope.member.firstName,
+    age: $scope.student.age,
+    ville: $scope.student.ville,
+    photo: $scope.photo,
+    tags: $scope.student.tags,
+    description: $scope.student.description,
+    Sexe: $scope.student.Sexe,
+    SpecialiteUn: $scope.student.SpecialiteUn,
+    SpecialiteDeux: $scope.student.SpecialiteDeux,
+    SpecialiteTrois: $scope.student.SpecialiteTrois,
+    Github: $scope.student.Github,
+    Linkedin: $scope.student.Linkedin,
+    Portfolio: $scope.student.Portfolio,
+    CV: $scope.student.CV,
+    Twitter: $scope.student.Twitter,
+    StackOverFlow: $scope.student.StackOverFlow,
+    Mail: $scope.member.email,
+    Contrat: $scope.student.Contrat,
+    DatePromo: $scope.student.DatePromo,
+    Domaine: $scope.student.Domaine,
+    ProjetUn: $scope.student.ProjetUn,
+    ProjetDeux: $scope.student.ProjetDeux,
+    ProjetTrois: $scope.student.ProjetTrois
     };
     console.log(dataStudent);
     serviceStudent.addStudent(dataStudent).then((response) => {
@@ -155,12 +154,11 @@ app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService'
       $state.reload();
     }, (err) => {
       console.log("Error");
-    });
+      });
   }
   $scope.updateStudent = (id) => {
     const response = confirm("Voulez vous vraiment modifier les infos de cet apprenant?");
     if (response === true) {
-      console.log($scope.student.ville);
       const newInfos = {
         nom: $scope.student.nom,
         prenom: $scope.student.prenom,
@@ -192,6 +190,18 @@ app.controller('profilsUserCtrl',['$http', '$scope', '$rootScope', 'AuthService'
       serviceStudent.updateStudent(id, newInfos).then((response) => {})
       alert("Apprenant modifié!")
     };
+  };
+
+  $scope.submitFiche = function(){
+    console.log($scope.cardExist);
+    if (!$scope.cardExist){
+      $scope.updateStudent()
+          console.log("j'update");
+    } else {
+        $scope.createSimplonien()
+            console.log('je submit New');
+
+    }
   };
 
   // Photo Upload
