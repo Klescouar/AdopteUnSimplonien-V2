3b4a5776af8754ae718f0b234f3da65591fef69f
Poncho <lescouarneckevin@yahoo.fr>
2017-02-15 22:35:21 +0100

    Add filter size for pic upload

--- app/controllers/controllers.upload.js
+++ app/controllers/controllers.upload.js
@@ -8,18 +8,11 @@ exports.upload = function(req, res) {
     var photos = [],
         form = new formidable.IncomingForm();
 
-    // Tells formidable that there will be multiple files sent.
-    form.multiples = true;
     // Upload directory for the images
     form.uploadDir = path.join(__dirname, 'tmp_uploads');
 
     // Invoked when a file has finished uploading.
     form.on('file', function(name, file) {
-        // Allow only 3 files to be uploaded.
-        if (photos.length === 3) {
-            fs.unlink(file.path);
-            return true;
-        }
 
         var buffer = null,
             type = null,
@@ -27,10 +20,10 @@ exports.upload = function(req, res) {
 
         buffer = readChunk.sync(file.path, 0, 262);
         type = fileType(buffer);
-
+        console.log(file.size);
         // Check the file type, must be either png,jpg or jpeg
         if (type !== null && (type.ext === 'png' || type.ext === 'jpg' || type.ext === 'jpeg')) {
-            if (file.size < 15000) {
+            if (file.size < 20000 || file.size > 100000) {
                 form.on('error', function(err) {
                     console.log('Problem size');
                 });
